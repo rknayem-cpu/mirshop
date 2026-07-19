8b3326fd66d84d0457ce5a9c48999f4acc7aa2b5
rknayem-cpu <nayemlx.mab@gmail.com>
2026-07-19 22:41:28 +0600

    favicon add

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const favicon = require('serve-favicon');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +21,9 @@ var app = express();
 
 
 connectDB();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
